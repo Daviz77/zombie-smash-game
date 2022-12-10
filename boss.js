1ee63952879a9bac7ba4212008dbfbc16dbe2eef
Daviz77 <daviddlcalle @gmail.com>
2022-12-10 12:57:40 +0100

    feat: Boss Shooting

--- boss.js
+++ boss.js
@@ -7,8 +7,11 @@ class Boss {
 		this.vx = speed
 		this.width = width
 		this.lifes = lifes
+		this.bullets = []
 		this.img = new Image()
 		this.img.src = "./images/HD_Zomboss.webp"
+		this.bullets.img = new Image()
+		this.bullets.src = "./images/moco.png"
 		this.isReady = false
 		this.img.onload = () => {
 			this.isReady = true
@@ -18,15 +21,19 @@ class Boss {
 
 	draw() {
 		if (this.isReady) {
-      const diff = this.height + this.y - 710
+			const xSpeed = 6
+			const ySpeed = 6
+			const diff = this.height + this.y - 710
 			if (diff > 0) {
 				this.y = 710 - this.height
 			}
 			this.ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+			}
+			this.bullets.forEach((bullet) => bullet.draw())
 		}
-	}
 
 	move(playerX, playerY) {
+		console.log(this.x, this.y)
 		if (this.isReady) {
 			if (playerX <= this.x) {
 				const diffX = this.x - playerX
@@ -36,7 +43,6 @@ class Boss {
 				} else {
 					this.x -= this.vx
 				}
-
 			} else {
 				const diffX = playerX - this.x
 
@@ -55,16 +61,20 @@ class Boss {
 				} else {
 					this.y -= this.vy
 				}
-
 			} else {
 				const diffY = playerY - this.y
-				
+
 				if (diffY < this.vy) {
 					this.y += diffY
 				} else {
 					this.y += this.vy
 				}
 			}
+		}
+
+		this.bullets.forEach((bullet) => bullet.move())
+		if (game.tick % 60 === 0){
+			this.isShooting(playerX, playerY)
 		}
 	}
 
@@ -74,6 +84,26 @@ class Boss {
 			this.x + this.width > obj.x &&
 			this.y < obj.y + obj.height &&
 			this.y + this.height > obj.y
+		)
+	}
+	isShooting(playerX, playerY) {
+		const rect = canvas.getBoundingClientRect()
+
+		const bossX = playerX - rect.left
+		const bossy = playerY - rect.top
+
+		const normalizedY = this.y + 75
+		const normalizedX = this.x - 100
+
+		const diffX = bossX - playerX
+		const diffY = bossy - playerY
+		const tanNum = Math.atan2(diffY, diffX)
+
+		let vfx = Math.cos(tanNum)
+		let vfy = Math.sin(tanNum)
+
+		this.bullets.push(
+			new Bullet(this.ctx, normalizedX + this.width, normalizedY, vfx, vfy)
 		)
 	}
 }
--- bullet.js
+++ bullet.js
@@ -3,8 +3,8 @@ class Bullet {
 		this.ctx = ctx
 		this.x = x
 		this.y = y
-		this.height = 5
-		this.width = 5
+		this.height = 7
+		this.width = 7
 
 		this.img = new Image()
 		this.img.src = "./images/PngItem_709207.png"
@@ -13,21 +13,19 @@ class Bullet {
 			this.isReady = true
 		}
 
-		this.xSpeed = 8 * xSpeed;
-		this.ySpeed = 8 * ySpeed;
+		this.xSpeed = 8 * xSpeed
+		this.ySpeed = 8 * ySpeed
 	}
 
 	draw() {
 		if (this.isReady) {
-			//this.ctx.beginPath();
 			this.ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
-			//this.ctx.stroke()
 		}
 	}
 
 	move() {
-		this.x += this.xSpeed;
-		this.y += this.ySpeed;
+		this.x += this.xSpeed
+		this.y += this.ySpeed
 	}
 
 	isColliding(obj) {
